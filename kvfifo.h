66ba236aa7d0068d29200aae5af842f2554570d2
LaMavia <tosurowiec@gmail.com>
2022-12-08 19:51:00 +0100

    change A, B to shared_ptr

--- kvfifo.h
+++ kvfifo.h
@@ -2,8 +2,10 @@
 #define KVFIFO_H
 
 #include <cstddef>
+#include <iostream>
 #include <list>
 #include <map>
+#include <memory>
 #include <stdexcept>
 #include <utility>
 
@@ -63,14 +65,14 @@ private:
     inline pointer operator->() { return &(it->first); }
   };
 
-  std::map<K, std::list<x>> A;
-  std::list<std::pair<K, V>> B;
+  std::shared_ptr<std::map<K, std::list<x>>> A;
+  std::shared_ptr<std::list<std::pair<K, V>>> B;
   bool is_a_copy;
 
   inline void copy() {
     if (is_a_copy) {
       kvfifo new_this{};
-      for (const auto &[key, val] : B) {
+      for (const auto &[key, val] : *B) {
         new_this.push(key, val);
       }
 
@@ -79,7 +81,9 @@ private:
   }
 
 public:
-  inline kvfifo() : A({}), B({}), is_a_copy(false) {}
+  inline kvfifo()
+      : A(std::make_shared<std::map<K, std::list<x>>>()),
+        B(std::make_shared<std::list<std::pair<K, V>>>()), is_a_copy(false) {}
   inline kvfifo(kvfifo const &other)
       : A(other.A), B(other.B), is_a_copy(true) {}
   inline kvfifo(kvfifo &&other) {
@@ -99,153 +103,153 @@ public:
   inline void push(K const &key, V const &val) {
     copy();
 
-    B.emplace_back(key, val);
-    A[key].push_back(--B.end());
+    B->emplace_back(key, val);
+    (*A)[key].push_back(--(B->end()));
   }
 
   inline void pop() {
-    if (B.empty()) {
+    if (B->empty()) {
       throw std::invalid_argument("");
     }
 
     copy();
 
-    auto key = B.front().first;
-    B.pop_front();
+    auto key = B->front().first;
+    B->pop_front();
 
-    auto &bucket = A[key];
+    auto &bucket = (*A)[key];
     bucket.pop_front();
     if (bucket.empty()) {
-      A.erase(key);
+      A->erase(key);
     }
   }
 
   inline void pop(K const &key) {
-    if (!A.contains(key)) {
+    if (!A->contains(key)) {
       throw std::invalid_argument("");
     }
 
     copy();
 
-    auto &bucket = A[key];
-    B.erase(bucket.front());
+    auto &bucket = (*A)[key];
+    B->erase(bucket.front());
     bucket.pop_front();
   }
 
   inline void move_to_back(K const &key) {
-    if (!A.contains(key)) {
+    if (!A->contains(key)) {
       throw std::invalid_argument("");
     }
 
     copy();
 
-    auto &bucket = A[key];
+    auto &bucket = (*A)[key];
     for (auto &it : bucket) {
-      B.push_back(*it);
-      B.erase(it);
-      it = --B.end();
+      B->push_back(*it);
+      B->erase(it);
+      it = --(B->end());
     }
   }
 
   inline std::pair<K const &, V &> front() {
-    if (B.empty()) {
+    if (B->empty()) {
       throw std::invalid_argument("");
     }
 
     copy();
 
-    auto &[key, val] = B.front();
+    auto &[key, val] = B->front();
     return {key, val};
   }
 
   inline std::pair<K const &, V const &> front() const {
-    if (B.empty()) {
+    if (B->empty()) {
       throw std::invalid_argument("");
     }
 
-    auto &[key, val] = B.front();
+    auto &[key, val] = B->front();
     return {key, val};
   }
   inline std::pair<K const &, V &> back() {
-    if (B.empty()) {
+    if (B->empty()) {
       throw std::invalid_argument("");
     }
 
     copy();
 
-    auto &[key, val] = B.back();
+    auto &[key, val] = B->back();
     return {key, val};
   }
 
   inline std::pair<K const &, V const &> back() const {
-    if (B.empty()) {
+    if (B->empty()) {
       throw std::invalid_argument("");
     }
 
-    auto &[key, val] = B.back();
+    auto &[key, val] = B->back();
     return {key, val};
   }
 
   inline std::pair<K const &, V &> first(K const &key) {
-    if (!A.contains(key)) {
+    if (!A->contains(key)) {
       throw std::invalid_argument("");
     }
 
     copy();
 
-    auto &it = A[key].front();
+    auto &it = (*A)[key].front();
     auto &val = it->second;
     return {key, val};
   }
 
   inline std::pair<K const &, V const &> first(K const &key) const {
-    if (!A.contains(key)) {
+    if (!A->contains(key)) {
       throw std::invalid_argument("");
     }
 
-    auto &it = A.find(key)->second.front();
+    auto &it = A->find(key)->second.front();
     auto &val = it->second;
     return {key, val};
   }
 
   inline std::pair<K const &, V &> last(K const &key) {
-    if (!A.contains(key)) {
+    if (!A->contains(key)) {
       throw std::invalid_argument("");
     }
 
     copy();
 
-    auto &it = A[key].back();
+    auto &it = (*A)[key].back();
     auto &val = it->second;
     return {key, val};
   }
 
   inline std::pair<K const &, V const &> last(K const &key) const {
-    if (!A.contains(key)) {
+    if (!A->contains(key)) {
       throw std::invalid_argument("");
     }
 
-    auto &it = A.find(key)->second.back();
+    auto &it = A->find(key)->second.back();
     auto &val = it->second;
     return {key, val};
   }
 
-  inline size_t size() const noexcept { return B.size(); }
+  inline size_t size() const noexcept { return B->size(); }
 
-  inline bool empty() const noexcept { return B.empty(); }
+  inline bool empty() const noexcept { return B->empty(); }
 
   inline size_t count(K const &key) const noexcept {
-    return A.contains(key) ? A.find(key)->second.size() : 0;
+    return A->contains(key) ? A->find(key)->second.size() : 0;
   };
 
   inline void clear() noexcept {
     copy();
-    A.clear();
-    B.clear();
+    A->clear();
+    B->clear();
   }
 
-  inline k_iterator k_begin() const noexcept { return {A.begin()}; }
-  inline k_iterator k_end() const noexcept { return {A.end()}; }
+  inline k_iterator k_begin() const noexcept { return {A->begin()}; }
+  inline k_iterator k_end() const noexcept { return {A->end()}; }
 };
 
 #endif /* KVFIFO_H */
