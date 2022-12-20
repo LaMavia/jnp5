4387835ec58b87672915e67835a9eadab827a5cc
LaMavia <tosurowiec@gmail.com>
2022-12-20 17:08:14 +0100

    nsq;

--- tt.h
+++ tt.h
@@ -3,9 +3,10 @@
 
 #include "kvfifo.h"
 #include <cassert>
+#include <map>
 
 namespace ttt {
-  bool b = false;
+bool b = false;
 class mv {
 public:
   mv() = default;
@@ -33,13 +34,33 @@ public:
 };
 
 void tt_main() {
-  kvfifo<int, mv> q{};
-  for (size_t i = 0; i < 10; i++) {
-    q.push(i, {});
-  }
+  // kvfifo<int, mv> q{};
+  // for (size_t i = 0; i < 10; i++) {
+  //   q.push(i, {});
+  // }
 
-  b = true;
-  q.move_to_back(5);
+  // b = true;
+  // q.move_to_back(5);
+
+  // kvfifo<int, int> q({});
+  // for (int i = 0; i < 10; i++)
+  //   q.push(i & 1, i);
+
+  // q.move_to_back(0);
+
+  // for (int i = 0; i < 10; i++) {
+  //   auto p = q.front();
+  //   q.pop();
+  //   printf("%d ", p.second);
+  // }
+
+  std::map<int, int> m0{};
+  m0.insert({{1, 1}, {2, 2}, {3, 3}, {4, 4}});
+
+  std::map<int, int> m1{m0};
+  m1.insert({{6, 6}});
+
+  assert(m1.begin() != m0.begin());
 }
 
 } // namespace ttt
